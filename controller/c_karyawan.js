4a8a98b3e3f3e19e3a4c9c3dfa179dc7bdfc36cb
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-03-05 20:53:24 +0700

    menu karyawan detail

--- controller/c_karyawan.js
+++ controller/c_karyawan.js
@@ -1,5 +1,6 @@
 const moment        = require('moment')
 const m_karyawan    = require('../model/m_karyawan')
+const db            = require('../config/database').db //koneksi database
 
 moment.locale('id')
 
@@ -12,6 +13,32 @@ async function all(req,res) {
 }
 
 
+async function detail(req,res) {
+    let id_kry = req.params.id_karyawan
+    let data_dari_sql;
+
+    try {
+        data_dari_sql = await db.query(`
+            SELECT
+                karyawan.*, departemen.nama as departemen_nama
+            FROM karyawan
+            LEFT JOIN departemen ON departemen.id = karyawan.departemen
+            WHERE karyawan.id = $1
+            ORDER BY nama ASC`,
+            [id_kry]
+        )
+    } catch (error) {
+        console.log(error)
+    }
+
+    res.render('karyawan/detail', {
+        data_karyawan: data_dari_sql.rows,
+        moment: moment,
+    })
+}
+
+
 module.exports = {
-    all
+    all,
+    detail
 }
