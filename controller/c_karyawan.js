908eabd64ffbf7cf666f6dac44e6c56093e3c839
waladi.abdauh@gmail.com <waladi.abdauh@gmail.com>
2024-03-05 21:46:01 +0700

    update

--- controller/c_karyawan.js
+++ controller/c_karyawan.js
@@ -1,18 +1,5 @@
 const moment        = require('moment')
 const m_karyawan    = require('../model/m_karyawan')
-const db            = require('../config/database').db //koneksi database
-const Pool      = require('pg').Pool
-
-const db = new Pool({
-    host: 'localhost',
-    port: 5433,
-    database: 'db_coba',
-    user: 'postgres',
-    password: 'root',
-    max: 20,
-    idleTimeoutMillis: 30000, //30 detik
-    connectionTimeoutMillis: 2000, //2 detik
-})
 
 moment.locale('id')
 
