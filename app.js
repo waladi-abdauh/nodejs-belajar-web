b5c062e1fd090273623f4c2d8ac3749416dfe106
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-03-06 19:25:02 +0700

    perbaikan view beranda & karyawan all

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ const app           = express()
 const port          = 3000
 const c_pengalaman  = require('./controller/c_pengalaman')
 const c_karyawan    = require('./controller/c_karyawan')
+const c_beranda     = require('./controller/c_beranda')
+
 
 
 app.set('view engine', 'ejs')
 app.set('views', './view-ejs')
 
 
-app.get('/', (req,res)=>{
-    res.send('Hello World!')
-})
 
-
-app.get('/profil', (req,res)=>{
-    res.send('<h1>Halaman Profil</h1>')
-})
-
-
+app.get('/', c_beranda.index)
 app.get('/pengalaman', c_pengalaman.render_pengalaman)
 app.get('/karyawan/all', c_karyawan.all )
 app.get('/karyawan/detail/:id_karyawan', c_karyawan.detail)
